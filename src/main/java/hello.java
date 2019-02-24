b6f3a691ce1bf2d76d3b2e57d1637d750a6f13b3
linyu <lin296633940@qq.com>
2019-02-24 10:29:09 +0800

    第一次提交

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+import javax.sound.midi.Soundbank;
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+
+
+}
